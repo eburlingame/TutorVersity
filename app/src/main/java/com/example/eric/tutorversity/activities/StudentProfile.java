2f1fbb2c879c460a2d14064790beb6e80a0d66b7
Mnagtode <nagtode62@gmail.com>
2017-06-07 21:27:41 -0700

    Student and Tutor Profile Pages (Issue Number: 675159)

---
+++ app/src/main/java/com/example/eric/tutorversity/activities/StudentProfile.java
@@ -0,0 +1,20 @@
+package com.example.eric.tutorversity.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.example.eric.tutorversity.R;
+
+/**
+ * Created by Manasi on 5/11/17.
+ */
+
+public class StudentProfile extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.student_profile);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/eric/tutorversity/activities/TutorProfile.java
@@ -0,0 +1,17 @@
+package com.example.eric.tutorversity.activities;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.example.eric.tutorversity.R;
+/**
+ * Created by Manasi on 6/7/17.
+ */
+
+public class TutorProfile {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tutor_profile);
+
+
+    }
+}
