2632cc06a8e1aafd2d834831704e47247ca08227
Eric Burlingame <eric@burlingames.com>
2017-06-07 22:36:56 -0700

    added travis.yml

---
+++ app/src/main/java/com/example/eric/tutorversity/activities/NearbyTutors.java
@@ -0,0 +1,15 @@
+package com.example.eric.tutorversity.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.eric.tutorversity.R;
+
+public class NearbyTutors extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nearby_tutors);
+    }
+}
